78b64cdf67b75c2875fc447fdc330cf8b69e1692
Andres B <andresblasiche@gmail.com>
2025-07-10 22:01:01 -0300

    Agrego manejo de errores

--- taskslist/src/pages/api/tasks/index.ts
+++ taskslist/src/pages/api/tasks/index.ts
@@ -4,28 +4,39 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        const tasks = await prisma.task.findMany({ orderBy: { createdAt: 'desc' } })
-        return res.status(200).json(tasks)
+  if (req.method === 'GET') {
+    try {
+      const tasks = await prisma.task.findMany({ orderBy: { createdAt: 'desc' } })
+      return res.status(200).json(tasks)
+
+    } catch (error) {
+      console.error('Error al buscar la tarea:', error);
+      return res.status(404).json({ error: 'Tarea no encontrada' })
+    }
+  }
+
+  if (req.method === 'POST') {
+    const { title, description } = req.body
+    if (!title || typeof title !== 'string') {
+      return res.status(400).json({ error: 'Título inválido'})
     }
 
-    if (req.method === 'POST') {
-        const { title, description } = req.body
-        if (!title || typeof title !== 'string') {
-            return res.status(400).json({ error: 'Título inválido'})
-        }
-
-        if (!description || typeof description !== 'string') {
-          return res.status(400).json({ error: 'Descripción inválida'})
-        }
-
-        const task = await prisma.task.create({
-            data: { title, description },
-        })
-
-        return res.status(201).json(task)
+    if (!description || typeof description !== 'string') {
+      return res.status(400).json({ error: 'Descripción inválida'})
     }
 
-    res.setHeader('Allow', ['GET', 'POST'])
-    return res.status(405).end('Método ${req.method} no permitido')
+    try {
+      const task = await prisma.task.create({
+        data: { title, description },
+      })
+      return res.status(201).json(task)
+      
+    } catch (error) {
+      console.error('Error al crear la tarea:', error);
+      return res.status(500).json({ error: 'Tarea no creada' })
+    }
+  }
+
+  res.setHeader('Allow', ['GET', 'POST'])
+  return res.status(405).end('Método ${req.method} no permitido')
 }
